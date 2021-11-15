1e32375c307f64505e7620d451243466933a4ff7
Thomas Metten <t.metten@aca-it.be>
2021-11-15 16:52:37 +0100

    check for changes before committing and pushing

--- utils/commit-readme.sh
+++ utils/commit-readme.sh
@@ -6,5 +6,7 @@
 #   \___\___/|_| |_| |_|_| |_| |_|_|\__|    |_|  \___|\__,_|\__,_|_| |_| |_|\___|
 #
 #- from github.com/tmttn/home-assistant-config
-git commit -m "update automatically generated part of README.md ✏️" README.md
-git push
+if ! git diff --quiet
+  git commit -m "update automatically generated part of README.md ✏️" README.md
+  git push
+fi
