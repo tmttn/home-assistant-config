e95ab547de17076f7d74108a01145cee06eddcb8
Thomas Metten <t.metten@aca-it.be>
2021-11-18 21:02:26 +0100

    also commit ascii comments

--- utils/commit-updates.py
+++ utils/commit-updates.py
@@ -63,6 +63,5 @@ if lovelace_update:
     subprocess.run(f"git add lovelace-ui.yaml".split())
     subprocess.run(["git", "commit", "-m", f"update lovelace-ui via UI"])
 
-
 if folder_update or ha_update or lovelace_update:
     subprocess.run(["git", "push"])
