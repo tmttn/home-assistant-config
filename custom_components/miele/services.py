190fa45a048aeed81a987b168afda30eb7355873
Thomas Metten <thomas.metten@gmail.com>
2022-12-14 22:45:00 +0100

    update custom_components/

---
+++ custom_components/miele/services.py
@@ -0,0 +1,215 @@
+"""Services for Miele integration."""
+
+import logging
+
+import aiohttp
+from homeassistant.const import CONF_DEVICE_ID, CONF_ENTITY_ID
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import device_registry
+from homeassistant.helpers.service import async_extract_config_entry_ids
+import voluptuous as vol
+
+from .const import (
+    AMBIENT_COLORS,
+    API,
+    COLORS,
+    DEVICE_NAME,
+    DOMAIN,
+    LIGHT,
+    MODES,
+    POWER_OFF,
+    POWER_ON,
+    PROCESS_ACTION,
+    PROCESS_ACTIONS,
+    PROGRAM_ID,
+    START_TIME,
+    TARGET_TEMPERATURE,
+    VENTILATION_STEP,
+)
+
+SERVICE_PROCESS_ACTION = cv.make_entity_service_schema(
+    {
+        vol.Required("action"): vol.In(PROCESS_ACTIONS),
+    },
+)
+
+GROUP_SGA = "sga"
+MSG1 = "Only one parameter allowed"
+
+SERVICE_GENERIC_ACTION = vol.All(
+    cv.make_entity_service_schema(
+        {
+            vol.Exclusive(PROCESS_ACTION, GROUP_SGA, msg=MSG1): vol.All(
+                int, vol.Range(min=1, max=10)
+            ),
+            vol.Exclusive(LIGHT, GROUP_SGA, msg=MSG1): vol.All(
+                int, vol.Range(min=1, max=2)
+            ),
+            vol.Exclusive(START_TIME, GROUP_SGA, msg=MSG1): list,
+            vol.Exclusive(PROGRAM_ID, GROUP_SGA, msg=MSG1): cv.positive_int,
+            vol.Exclusive(VENTILATION_STEP, GROUP_SGA, msg=MSG1): vol.All(
+                int, vol.Range(min=1, max=4)
+            ),
+            vol.Exclusive(TARGET_TEMPERATURE, GROUP_SGA, msg=MSG1): dict,
+            vol.Exclusive(DEVICE_NAME, GROUP_SGA, msg=MSG1): cv.string,
+            vol.Exclusive(POWER_ON, GROUP_SGA, msg=MSG1): cv.boolean,
+            vol.Exclusive(POWER_OFF, GROUP_SGA, msg=MSG1): cv.boolean,
+            vol.Exclusive(COLORS, GROUP_SGA, msg=MSG1): vol.In(AMBIENT_COLORS),
+            vol.Exclusive(MODES, GROUP_SGA, msg=MSG1): cv.positive_int,
+        },
+    ),
+    cv.has_at_least_one_key(
+        PROCESS_ACTION,
+        LIGHT,
+        START_TIME,
+        PROGRAM_ID,
+        VENTILATION_STEP,
+        TARGET_TEMPERATURE,
+        DEVICE_NAME,
+        POWER_ON,
+        POWER_OFF,
+        COLORS,
+        MODES,
+    ),
+)
+
+SERVICE_RAW = vol.Schema(
+    {
+        vol.Required("serialno"): cv.string,
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+SERVICE_PROGRAM = cv.make_entity_service_schema(
+    {
+        vol.Required(PROGRAM_ID): cv.positive_int,
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_services(hass: HomeAssistant) -> None:
+    """Set up services."""
+
+    async def extract_our_config_entry_ids(service_call: ServiceCall):
+        return [
+            entry_id
+            for entry_id in await async_extract_config_entry_ids(hass, service_call)
+            if (entry := hass.config_entries.async_get_entry(entry_id))
+            and entry.domain == DOMAIN
+        ]
+
+    async def send_process_action(call: ServiceCall):
+        _LOGGER.debug("Call: %s", call)
+        if not (our_entry_ids := await extract_our_config_entry_ids(call)):
+            raise HomeAssistantError(
+                "Failed to call service 'process_action'. Config entry for target not found"
+            )
+        _LOGGER.debug("Entries: %s", our_entry_ids)
+        device_reg = device_registry.async_get(hass)
+        for ent in call.data[CONF_DEVICE_ID]:
+            device_entry = device_reg.async_get(ent)
+            for ident in device_entry.identifiers:
+                for val in ident:
+                    if val != DOMAIN:
+                        serno = val
+
+            _api = hass.data[DOMAIN][our_entry_ids[0]][API]
+            act = PROCESS_ACTIONS[call.data["action"]]
+            try:
+                await _api.send_action(serno, {PROCESS_ACTION: act})
+            except aiohttp.ClientResponseError as ex:
+                raise HomeAssistantError(
+                    f"Service generic_action: {ex.status} {ex.message}"
+                )
+        return
+
+    async def send_generic_action(call: ServiceCall):
+        _LOGGER.debug("Call: %s", call)
+        if not (our_entry_ids := await extract_our_config_entry_ids(call)):
+            raise HomeAssistantError(
+                "Failed to call service 'generic_action'. Config entry for target not found."
+            )
+        if CONF_DEVICE_ID not in call.data.keys():
+            raise HomeAssistantError(
+                "Cannot call generic_action on entity. Only on device."
+            )
+        _LOGGER.debug("Entries: %s", our_entry_ids)
+        device_reg = device_registry.async_get(hass)
+        for ent in call.data[CONF_DEVICE_ID]:
+            device_entry = device_reg.async_get(ent)
+            for ident in device_entry.identifiers:
+                for val in ident:
+                    if val != DOMAIN:
+                        serno = val
+
+            _api = hass.data[DOMAIN][our_entry_ids[0]][API]
+            data = call.data.copy()
+            if CONF_ENTITY_ID in data.keys():
+                data.pop(CONF_ENTITY_ID)
+            if CONF_DEVICE_ID in data.keys():
+                data.pop(CONF_DEVICE_ID)
+            try:
+                await _api.send_action(serno, data)
+            except aiohttp.ClientResponseError as ex:
+                raise HomeAssistantError(
+                    f"Service generic_action: {ex.status} {ex.message}"
+                )
+        return
+
+    async def send_raw(call: ServiceCall):
+        _LOGGER.debug("Call: %s", call)
+        account = list(hass.data[DOMAIN].keys())[0]
+        _api = hass.data[DOMAIN][account][API]
+        try:
+            await _api.send_action(call.data["serialno"], call.data["extra"])
+        except aiohttp.ClientResponseError as ex:
+            raise HomeAssistantError(f"Service raw: {ex.status} {ex.message}")
+        return
+
+    async def set_program(call: ServiceCall):
+        _LOGGER.debug("Call: %s", call)
+        if not (our_entry_ids := await extract_our_config_entry_ids(call)):
+            raise HomeAssistantError(
+                "Failed to call service 'set_program'. Config entry for target not found."
+            )
+        if CONF_DEVICE_ID not in call.data.keys():
+            raise HomeAssistantError(
+                "Cannot call set_program on entity. Only on device."
+            )
+        _LOGGER.debug("Entries: %s", our_entry_ids)
+        device_reg = device_registry.async_get(hass)
+        for ent in call.data[CONF_DEVICE_ID]:
+            device_entry = device_reg.async_get(ent)
+            for ident in device_entry.identifiers:
+                for val in ident:
+                    if val != DOMAIN:
+                        serno = val
+
+            _api = hass.data[DOMAIN][our_entry_ids[0]][API]
+            data = call.data.copy()
+            if CONF_ENTITY_ID in data.keys():
+                data.pop(CONF_ENTITY_ID)
+            if CONF_DEVICE_ID in data.keys():
+                data.pop(CONF_DEVICE_ID)
+            try:
+                await _api.set_program(serno, data)
+            except aiohttp.ClientResponseError as ex:
+                raise HomeAssistantError(
+                    f"Service set_program: {ex.status} {ex.message}"
+                )
+        return
+
+    hass.services.async_register(
+        DOMAIN, "process_action", send_process_action, SERVICE_PROCESS_ACTION
+    )
+    hass.services.async_register(
+        DOMAIN, "generic_action", send_generic_action, SERVICE_GENERIC_ACTION
+    )
+    hass.services.async_register(DOMAIN, "raw", send_raw, SERVICE_RAW)
+    hass.services.async_register(DOMAIN, "set_program", set_program, SERVICE_PROGRAM)
+    return
---
+++ custom_components/miele/switch.py
@@ -0,0 +1,243 @@
+"""Platform for Miele switch integration."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+import logging
+from typing import Any, Final
+
+import aiohttp
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
+
+from . import get_coordinator
+from .const import (
+    ACT_START_SUPERCOOL,
+    ACT_START_SUPERFREEZE,
+    ACT_STOP_SUPERCOOL,
+    ACT_STOP_SUPERFREEZE,
+    ACTIONS,
+    API,
+    COFFEE_SYSTEM,
+    DIALOG_OVEN,
+    DISHWASHER,
+    DOMAIN,
+    FREEZER,
+    FRIDGE,
+    FRIDGE_FREEZER,
+    HOOD,
+    MICROWAVE,
+    OVEN,
+    OVEN_MICROWAVE,
+    POWER_OFF,
+    POWER_ON,
+    PROCESS_ACTION,
+    STEAM_OVEN,
+    STEAM_OVEN_COMBI,
+    STEAM_OVEN_MICRO,
+    TUMBLE_DRYER,
+    WASHER_DRYER,
+    WASHING_MACHINE,
+    WINE_CABINET_FREEZER,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class MieleSwitchDescription(SwitchEntityDescription):
+    """Class describing Miele switch entities."""
+
+    data_tag: str | None = None
+    type_key: str = "ident|type|value_localized"
+    on_value: int = 0
+    off_value: int = 0
+    on_data: dict[str, Any] | None = None
+    off_data: dict[str, Any] | None = None
+
+
+@dataclass
+class MieleSwitchDefinition:
+    """Class for defining switch entities."""
+
+    types: tuple[int, ...]
+    description: MieleSwitchDescription = None
+
+
+SWITCH_TYPES: Final[tuple[MieleSwitchDefinition, ...]] = (
+    MieleSwitchDefinition(
+        types=[FRIDGE, FRIDGE_FREEZER],
+        description=MieleSwitchDescription(
+            key="supercooling",
+            data_tag="state|status|value_raw",
+            on_value=14,
+            icon="mdi:snowflake",
+            name="Supercooling",
+            on_data={PROCESS_ACTION: ACT_START_SUPERCOOL},
+            off_data={PROCESS_ACTION: ACT_STOP_SUPERCOOL},
+        ),
+    ),
+    MieleSwitchDefinition(
+        types=[FREEZER, FRIDGE_FREEZER, WINE_CABINET_FREEZER],
+        description=MieleSwitchDescription(
+            key="superfreezing",
+            data_tag="state|status|value_raw",
+            on_value=13,
+            icon="mdi:snowflake",
+            name="Superfreezing",
+            on_data={PROCESS_ACTION: ACT_START_SUPERFREEZE},
+            off_data={PROCESS_ACTION: ACT_STOP_SUPERFREEZE},
+        ),
+    ),
+    MieleSwitchDefinition(
+        types=[
+            WASHING_MACHINE,
+            TUMBLE_DRYER,
+            DISHWASHER,
+            OVEN,
+            OVEN_MICROWAVE,
+            STEAM_OVEN,
+            MICROWAVE,
+            COFFEE_SYSTEM,
+            HOOD,
+            WASHER_DRYER,
+            STEAM_OVEN_COMBI,
+            STEAM_OVEN_MICRO,
+            DIALOG_OVEN,
+        ],
+        description=MieleSwitchDescription(
+            key="poweronoff",
+            data_tag="state|status|value_raw",
+            off_value=1,
+            icon="mdi:power",
+            name="Power on",
+            on_data={POWER_ON: True},
+            off_data={POWER_OFF: True},
+        ),
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the switch platform."""
+    coordinator = await get_coordinator(hass, config_entry)
+
+    entities = []
+    for idx, ent in enumerate(coordinator.data):
+        for definition in SWITCH_TYPES:
+            if coordinator.data[ent]["ident|type|value_raw"] in definition.types:
+                entities.append(
+                    MieleSwitch(
+                        coordinator,
+                        idx,
+                        ent,
+                        definition.description,
+                        hass,
+                        config_entry,
+                    )
+                )
+
+    async_add_entities(entities)
+
+
+class MieleSwitch(CoordinatorEntity, SwitchEntity):
+    """Representation of a Switch."""
+
+    entity_description: MieleSwitchDescription
+
+    def __init__(
+        self,
+        coordinator: DataUpdateCoordinator,
+        idx,
+        ent,
+        description: MieleSwitchDescription,
+        hass: HomeAssistant,
+        entry: ConfigType,
+    ):
+        """Initialize the switch."""
+        super().__init__(coordinator)
+        self._api = hass.data[DOMAIN][entry.entry_id][API]
+        self._api_data = hass.data[DOMAIN][entry.entry_id]
+
+        self._idx = idx
+        self._ent = ent
+        self.entity_description = description
+        _LOGGER.debug("init switch %s", ent)
+        appl_type = self.coordinator.data[self._ent][self.entity_description.type_key]
+        if appl_type == "":
+            appl_type = self.coordinator.data[self._ent][
+                "ident|deviceIdentLabel|techType"
+            ]
+        self._attr_name = self.entity_description.name
+        self._attr_has_entity_name = True
+        self._attr_unique_id = f"{self.entity_description.key}-{self._ent}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self._ent)},
+            name=appl_type,
+            manufacturer="Miele",
+            model=self.coordinator.data[self._ent]["ident|deviceIdentLabel|techType"],
+        )
+
+    @property
+    def is_on(self):
+        """Return the state of the switch."""
+        if self.entity_description.key in {"supercooling", "superfreezing"}:
+            return (
+                self.coordinator.data[self._ent][self.entity_description.data_tag]
+                == self.entity_description.on_value
+            )
+
+        elif self.entity_description.key in {"poweronoff"}:
+            power_data = (
+                self._api_data.get(ACTIONS, {}).get(self._ent, {}).get(POWER_OFF, True)
+            )
+            return power_data
+
+        return False
+
+    @property
+    def available(self):
+        """Return the availability of the entity."""
+
+        if not self.coordinator.last_update_success:
+            return False
+
+        if self.entity_description.key in {"poweronoff"}:
+            power_data = (
+                self._api_data.get(ACTIONS, {}).get(self._ent, {}).get(POWER_OFF, False)
+            ) or (
+                self._api_data.get(ACTIONS, {}).get(self._ent, {}).get(POWER_ON, False)
+            )
+            return power_data
+
+        return self.coordinator.data[self._ent]["state|status|value_raw"] != 255
+
+    async def async_turn_on(self, **kwargs):
+        """Turn on the device."""
+        _LOGGER.debug("turn_on -> kwargs: %s", kwargs)
+        try:
+            await self._api.send_action(self._ent, self.entity_description.on_data)
+        except aiohttp.ClientResponseError as ex:
+            _LOGGER.error("Turn_on: %s - %s", ex.status, ex.message)
+
+        # await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn off the device."""
+        _LOGGER.debug("turn_off -> kwargs: %s", kwargs)
+        try:
+            await self._api.send_action(self._ent, self.entity_description.off_data)
+        except aiohttp.ClientResponseError as ex:
+            _LOGGER.error("Turn_off: %s - %s", ex.status, ex.message)
+
+        # await self.coordinator.async_request_refresh()
